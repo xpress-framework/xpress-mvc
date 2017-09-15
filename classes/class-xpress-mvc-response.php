599f5d3f61a5ae858c55cf9677594f769438b541
Thiago Benvenuto <brazabr@gmail.com>
2017-09-15 14:44:37 +0000

    Implemented new static constructor to convert WP_HTTP_Response to XPress_MVC_Response.

--- classes/class-xpress-mvc-response.php
+++ classes/class-xpress-mvc-response.php
@@ -22,4 +22,15 @@ class XPress_MVC_Response extends WP_REST_Response {
 	 */
 	public $template = null;
 
+	/**
+	 * Static constructor that accepts an original WP_HTTP_Response and copy its values to the new XPress_MVC_Response.
+	 *
+     * @access public
+	 *
+	 * @param WP_HTTP_Response $original The original WP_HTTP_Response to copy values from.
+	 * @return  XPress_VMC_Response
+	 */
+	public static function from_wp_http_response( WP_HTTP_Response $original ) {
+		return new XPress_MVC_Response( $original->get_data(), $original->get_status(), $original->get_headers() );
+ 	}
 }
