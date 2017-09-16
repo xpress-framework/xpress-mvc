bc23d3ef91b9bfe084422f375ed748d1957a169c
Thiago Benvenuto <brazabr@gmail.com>
2017-09-16 14:05:04 -0300

    Started implementing tests for XPress_MVC_Server class porting some tests from WordPress oficial tests for WP_REST_Server class.

--- tests/fixtures/class-xpress-mvc-sample-controller.php
+++ tests/fixtures/class-xpress-mvc-sample-controller.php
@@ -14,4 +14,58 @@
  */
 class XPress_MVC_Sample_Controller extends XPress_MVC_Controller {
 
+	/**
+	 * Register the test routes.
+	 */
+	function register_routes() {
+		// WordPress REST Server ported tests.
+		$this->register_route( 'test-default-value', '/tests/default_value', array(
+			'methods' => array( 'GET' ),
+			'callback' => '__return_null',
+			'args'     => array(
+				'foo'  => array(
+					'default'  => 'bar',
+				),
+			),
+		) );
+
+		$this->register_route( 'test-optional-value', '/tests/optional_value', array(
+			'methods'  => array( 'GET' ),
+			'callback' => '__return_null',
+			'args'     => array(
+				'foo'  => array(),
+			),
+		) );
+
+		$this->register_route( 'test-head-request', '/tests/head_request', array(
+			'methods'  => array( 'GET' ),
+			'callback' => '__return_true',
+		) );
+
+		// XPress tests.
+		$this->register_route( 'test-invalid-argument', '/tests/invalid_argument', array(
+			'methods' => array( 'GET' ),
+			'callback' => array( $this, 'return_params_and_errors' ),
+			'args'     => array(
+				'foo'  => array(
+					'validate_callback' => function( $param, $request, $key ) {
+						return is_numeric( $param );
+					},
+				),
+			),
+		) );
+	}
+
+	/**
+	 * Callback to be used in tests.
+	 * @param  WP_REST_Request $request The request object.
+	 * @return array                    The array will contains two keys: params and errors, bypassed from the request object.
+	 */
+	public function return_params_and_errors( XPress_MVC_Request $request ) {
+		return array(
+			'params'     => $request->get_params(),
+			'has_errors' => $request->has_errors(),
+			'errors'     => $request->get_errors(),
+		);
+	}
 }
---
+++ tests/test-xpress-mvc-server.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * XPress MVC Controller Test
+ *
+ * @package    XPress
+ * @subpackage MVC
+ * @author     Thiago Benvenuto
+ * @license    GPLv2
+ * @since      0.2.0
+ */
+
+/**
+ * Server Test class.
+ */
+class XPress_MVC_Server_Test extends WP_UnitTestCase {
+	/**
+	 * Initialize XPress_MVC_Server.
+	 */
+	public function setUp() {
+		require_once 'fixtures/class-xpress-mvc-sample-controller.php';
+		$this->controller = new XPress_MVC_Sample_Controller();
+
+		// Reset MVC server to ensure only our routes are registered.
+		$GLOBALS['xpress_mvc_server'] = null;
+		$this->server = xpress_mvc_get_server();
+	}
+
+	/**
+	 * Test if a default value is used when not present in request.
+	 */
+	public function test_default_param() {
+		$request = new XPress_MVC_Request( 'GET', '/tests/default_value' );
+		$default = array(
+			'foo' => 'bar',
+		);
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertInstanceOf( 'XPress_MVC_Response', $response );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( $default, $request->get_params() );
+		$this->assertEquals( 'bar', $request['foo'] );
+	}
+
+	/**
+	 * Test if when define, a default value is overriden.
+	 */
+	public function test_default_param_is_overridden() {
+		$request = new XPress_MVC_Request( 'GET', '/tests/default_value' );
+		$request->set_query_params( array(
+			'foo' => 123,
+		) );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertInstanceOf( 'XPress_MVC_Response', $response );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( '123', $request['foo'] );
+	}
+
+	/**
+	 * Test if an optional value is not return when not present in request.
+	 */
+	public function test_optional_param() {
+		$request = new XPress_MVC_Request( 'GET', '/tests/optional_value' );
+		$request->set_query_params( array() );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertInstanceOf( 'XPress_MVC_Response', $response );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertArrayNotHasKey( 'foo', (array) $request );
+	}
+
+	/**
+	 * Test if GET route answers to HEAD requests.
+	 */
+	public function test_head_request_handled_by_get() {
+		$request = new XPress_MVC_Request( 'HEAD', '/tests/head_request' );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertEquals( 200, $response->get_status() );
+	}
+
+
+	/**
+	 * Ensure request is sent to callback in case of invalid arguments.
+	 */
+	public function test_invalid_argument() {
+		$request = new XPress_MVC_Request( 'GET', '/tests/invalid_argument' );
+		$request->set_query_params( array(
+		 	'foo' => 'abc',
+		) );
+		$errors = array(
+			'foo' => array(
+				'Invalid parameter.',
+		  	),
+		);
+		$result = array(
+			'params'     => $request->get_params(),
+			'has_errors' => true,
+			'errors'     => $errors,
+		);
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertInstanceOf( 'XPress_MVC_Response', $response );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( $result, $response->get_data() );
+	}
+}
